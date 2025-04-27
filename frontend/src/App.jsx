67a0dc40ef0394a53e4ecc6000d1f23f5dc962da
saumyajit <156281612+Saumyajit007@users.noreply.github.com>
2025-04-27 13:42:16 +0530

    improvement

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
-import Header from './components/Header.jsx'
+import React ,{lazy,Suspense}from 'react'
 import { Outlet } from 'react-router-dom'
-
+const Header=lazy(()=>import('./components/Header.jsx'))
 function App() {
 
   return (
     <div className='app min-h-screen min-w-screen bg-gray-200 text-slate-950  overflow-y-hidden overflow-x-hidden antialiased'>
         <div>
-        <Header />
+        <Suspense fallback={<></>}><Header /></Suspense>
         <Outlet />
         </div>
     </div>
